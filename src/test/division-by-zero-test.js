b996a840f3bea245f2d2539a651a084b667e88d3
John Haugeland <stonecypher@gmail.com>
2014-08-24 17:38:47 -0700

    suppressing jshint because of version problems; trying post hook; satisfying jshint on semicolon

--- src/test/division-by-zero-test.js
+++ src/test/division-by-zero-test.js
@@ -7,14 +7,14 @@ var vows = require('vows'),
 // Create a Test Suite
 vows.describe('Division by Zero').addBatch({
     'when dividing a number by zero': {
-        topic: function () { return 42 / 0 },
+        topic: function () { return 42 / 0; },
 
         'we get Infinity': function (topic) {
             assert.equal (topic, Infinity);
         }
     },
     'but when dividing zero by zero': {
-        topic: function () { return 0 / 0 },
+        topic: function () { return 0 / 0; },
 
         'we get a value which': {
             'is not a number': function (topic) {
